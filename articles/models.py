4213551b3108914c4d1a6f2644d45b1339feac2b
Murilo Camargos <murilo.camargosf@gmail.com>
2017-12-22 12:39:49 -0200

    Add and test category's model

--- articles/models.py
+++ articles/models.py
@@ -1,37 +1,12 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils import timezone
 
-class Outlet(models.Model):
-    """This class represents an information outlet."""
+class NameSlug(models.Model):
+    """This class is an abstraction for every model that needs a name and
+       its slugified version along with creation and changing dates."""
     name = models.CharField(max_length=255, blank=False, unique=True)
     slug = models.SlugField(max_length=255, unique=True)
-    website = models.CharField(max_length=255, blank=False, unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        """Return a human readable representation of the model instance."""
-        return "{}".format(self.name)
-
-    def save(self, *args, **kwargs):
-        """Overrides save method to slugify name by default."""
-        if not self.id and not self.slug:
-            # Slugify only new entries without a defined slug
-            self.slug = slugify(self.name)
-
-        super(Outlet, self).save(*args, **kwargs)
-
-
-class Author(models.Model):
-    """This class represents an author."""
-    name = models.CharField(max_length=255, blank=False, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
-    profile = models.CharField(max_length=255, unique=True, default='')
-    twitter = models.CharField(max_length=255, unique=True, default='')
-    linkedin = models.CharField(max_length=255, unique=True, default='')
-    facebook = models.CharField(max_length=255, unique=True, default='')
-    website = models.CharField(max_length=255, default='')
-    avatar = models.CharField(max_length=255, default='')
-    about = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -46,4 +21,26 @@ class Author(models.Model):
             # Slugify only new entries without a defined slug
             self.slug = slugify(self.name)
 
-        super(Author, self).save(*args, **kwargs)
+        super(NameSlug, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+class Outlet(NameSlug):
+    """This class represents an information outlet."""
+    website = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField()
+
+class Category(NameSlug):
+    """This class represents the article category."""
+    pass
+
+class Author(NameSlug):
+    """This class represents an author."""
+    profile = models.CharField(max_length=255, unique=True, default='')
+    twitter = models.CharField(max_length=255, unique=True, default='')
+    linkedin = models.CharField(max_length=255, unique=True, default='')
+    facebook = models.CharField(max_length=255, unique=True, default='')
+    website = models.CharField(max_length=255, default='')
+    avatar = models.CharField(max_length=255, default='')
+    about = models.TextField()
--- articles/tests/models.py
+++ articles/tests/models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Author, Outlet
+from ..models import Author, Outlet, Category, NameSlug
 
 class AuthorModelTestCase(TestCase):
     """This class defines the test suite for the Author model."""
@@ -60,3 +60,33 @@ class OutletModelTestCase(TestCase):
         self.outlet.slug = 'tcrunch'
         self.outlet.save()
         self.assertEqual(self.outlet.slug, 'tcrunch')
+
+class CategoryModelTestCase(TestCase):
+    """This class defines the test suite for the Category model."""
+
+    def setUp(self):
+        """Defines the test client and other test variables."""
+        self.name = "Culture"
+        self.category = Category(name = self.name)
+
+    def test_category_can_create(self):
+        """Tests if the category model can create a category."""
+        old_count = Category.objects.count()
+        self.category.save()
+        new_count = Category.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    def test_category_string_representation(self):
+        """Tests if the category is correctly represented."""
+        self.assertEqual(str(self.category), self.name)
+
+    def test_category_default_slug(self):
+        """Tests if the category's name is slugified by default."""
+        self.category.save()
+        self.assertEqual(self.category.slug, 'culture')
+
+    def test_category_defined_slug(self):
+        """Tests if the model accepts a user defined slug."""
+        self.category.slug = 'cult'
+        self.category.save()
+        self.assertEqual(self.category.slug, 'cult')
