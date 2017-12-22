cfe570a3e330fd37459a17f51e02d00e2a0416c1
Murilo Camargos <murilo.camargosf@gmail.com>
2017-12-22 09:31:15 -0200

    Remove .pyc files and change dbms to postgresql

--- tcrunch/tests/models.py
+++ tcrunch/tests/models.py
@@ -5,26 +5,28 @@ class AuthorModelTestCase(TestCase):
     """This class defines the test suite for the Author model."""
 
     def setUp(self):
-        """Define the test client and other test variables."""
+        """Defines the test client and other test variables."""
         self.name = "Murilo Camargos"
         self.author = Author(name = self.name)
 
     def test_author_can_create(self):
-        """Test if the author model can create an author."""
+        """Tests if the author model can create an author."""
         old_count = Author.objects.count()
         self.author.save()
         new_count = Author.objects.count()
         self.assertNotEqual(old_count, new_count)
 
     def test_author_string_representation(self):
-        """Test if the author is correctly represented."""
+        """Tests if the author is correctly represented."""
         self.assertEqual(str(self.author), self.name)
 
     def test_author_default_slug(self):
+        """Tests if the author's name is slugyfied by default."""
         self.author.save()
         self.assertEqual(self.author.slug, 'murilo-camargos')
 
     def test_author_defined_slug(self):
+        """Tests if the model accepts a user defined slug."""
         self.author.slug = 'john-doe'
         self.author.save()
         self.assertEqual(self.author.slug, 'john-doe')
--- tcscraper/settings.py
+++ tcscraper/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'tcscraper.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tcscraper',
+        'USER': 'tcscraper',
+        'PASSWORD': 'tcscraper',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
