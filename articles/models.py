bdf55a330db5066320c34c32f15f0db4d2654371
Murilo Camargos <murilo.camargosf@gmail.com>
2017-12-22 12:12:17 -0200

    Add and test outlet's model

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+class Outlet(models.Model):
+    """This class represents an information outlet."""
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    website = models.CharField(max_length=255, blank=False, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
+
+    def save(self, *args, **kwargs):
+        """Overrides save method to slugify name by default."""
+        if not self.id and not self.slug:
+            # Slugify only new entries without a defined slug
+            self.slug = slugify(self.name)
+
+        super(Outlet, self).save(*args, **kwargs)
+
+
 class Author(models.Model):
     """This class represents an author."""
     name = models.CharField(max_length=255, blank=False, unique=True)
--- articles/tests/models.py
+++ articles/tests/models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Author
+from ..models import Author, Outlet
 
 class AuthorModelTestCase(TestCase):
     """This class defines the test suite for the Author model."""
@@ -21,7 +21,7 @@ class AuthorModelTestCase(TestCase):
         self.assertEqual(str(self.author), self.name)
 
     def test_author_default_slug(self):
-        """Tests if the author's name is slugyfied by default."""
+        """Tests if the author's name is slugified by default."""
         self.author.save()
         self.assertEqual(self.author.slug, 'murilo-camargos')
 
@@ -30,3 +30,33 @@ class AuthorModelTestCase(TestCase):
         self.author.slug = 'john-doe'
         self.author.save()
         self.assertEqual(self.author.slug, 'john-doe')
+
+class OutletModelTestCase(TestCase):
+    """This class defines the test suite for the Outlet model."""
+
+    def setUp(self):
+        """Defines the test client and other test variables."""
+        self.name = "Tech Crunch"
+        self.outlet = Outlet(name = self.name)
+
+    def test_outlet_can_create(self):
+        """Tests if the outlet model can create an outlet."""
+        old_count = Outlet.objects.count()
+        self.outlet.save()
+        new_count = Outlet.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    def test_outlet_string_representation(self):
+        """Tests if the outlet is correctly represented."""
+        self.assertEqual(str(self.outlet), self.name)
+
+    def test_outlet_default_slug(self):
+        """Tests if the outlet's name is slugified by default."""
+        self.outlet.save()
+        self.assertEqual(self.outlet.slug, 'tech-crunch')
+
+    def test_outlet_defined_slug(self):
+        """Tests if the model accepts a user defined slug."""
+        self.outlet.slug = 'tcrunch'
+        self.outlet.save()
+        self.assertEqual(self.outlet.slug, 'tcrunch')
