ad24949a039ff799fbdd7219507705249b4f974b
Murilo Camargos <murilo.camargosf@gmail.com>
2017-12-22 19:53:44 -0200

    Add article creator helper function

--- articles/tests/utils.py
+++ articles/tests/utils.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from articles import utils
 from lxml import etree
-import os
+from articles.models import Outlet, Author, Category, Article
+import os, dateutil.parser
 
 class UtilsTestCase(TestCase):
     """This class defines the test suite for the utility functions."""
@@ -35,3 +36,32 @@ class UtilsTestCase(TestCase):
         # Get attribute named `url` from the item's children named `media:thumbnail`
         thumbs = utils.get_text_or_attr(item, 'media:thumbnail', 'url')
         self.assertEqual(thumbs, ['https://tctechcrunch2011.files.wordpress.com/2017/12/gettyimages-170409877.jpg?w=210&h=158&crop=1', 'https://tctechcrunch2011.files.wordpress.com/2017/12/gettyimages-170409877.jpg'])
+
+    def test_utils_create_article(self):
+        """Tests if article creator helper is adding an article and its
+           properties such as categories and author."""
+        outlet = Outlet.objects.create(name = 'TechCrunch')
+        article = {
+            'title': 'My article',
+            'url': 'http://www.myarticle.com.br',
+            'date': dateutil.parser.parse('Fri, 22 Dec 2017 17:21:29 +0000'),
+            'thumb': 'thumbnail.jpg',
+            'content': 'Article\'s content',
+            'author': {
+                'name': 'Murilo Camargos',
+                'avatar': ''
+            },
+            'categories': ['IT', 'Django']
+        }
+
+        article = utils.create_article(outlet, article)
+
+        # Check if article if an Article object
+        self.assertEqual(isinstance(article, Article), True)
+        self.assertEqual(Article.objects.count(), 1)
+
+        # Check if categories were created
+        self.assertEqual(Category.objects.count(), 2)
+
+        # Check if author was created
+        self.assertEqual(Author.objects.count(), 1)
--- articles/utils.py
+++ articles/utils.py
@@ -1,3 +1,54 @@
+from django.shortcuts import get_object_or_404
+from .models import Author, Category, Outlet, Article
+
+def create_article(outlet, data):
+    """
+    This function creates an article with all its dependencies, such as,
+    its author and categories.
+
+    Args:
+        outlet:  an Outlet object to create/find the author
+        data: a dictionary with the article's attributes, with the
+                 following structure:
+                  - title
+                  - url
+                  - date
+                  - thumb
+                  - content
+                  - author
+                    - name
+                    - avatar
+                  - categories (a list of categories' names)
+    """
+    if data['date'] == '' or data['title'] == '':
+        return None
+
+    author, created = Author.objects.get_or_create(
+        name = data['author']['name'],
+        outlet_id = outlet.id,
+        defaults = {
+            'avatar': data['author']['avatar']
+        }
+    )
+
+    article, created = Article.objects.get_or_create(
+        url = data['url'],
+        defaults = {
+            'title': data['title'],
+            'date': data['date'],
+            'thumb': data['thumb'],
+            'content': data['content'],
+            'author_id': author.id,
+            'outlet_id': outlet.id
+        }
+    )
+
+    for cat_name in data['categories']:
+        category, created = Category.objects.get_or_create(name = cat_name)
+        article.categories.add(category)
+
+    return article
+
 def get_text_or_attr(item, key, attr = None):
     """
     This function returns a string or a list of strings containing
